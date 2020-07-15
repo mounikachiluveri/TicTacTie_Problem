bd7283ce85f6b440525c06d090a5c2571397a1cd
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-15 23:44:55 +0530

    [Mounika] Added:Assign the symbol to the player

--- TicTacTie.sh
+++ TicTacTie.sh
@@ -6,14 +6,34 @@ switchPlayer=1
 playerLetter=O
 computerLetter=X
 declare -a board
- function resettingBoard() {
-        board=(. . . . . . . . . .)
+function resettingBoard() {
+    board=(. . . . . . . . . .)
 }
 
- function displayBoard() {
-
-        echo -e "****** Tic-Tac-Toe ******* \n|  ${board[1]}  ||  ${board[2]}  ||  ${board[3]}  |\n---------------------\n| ${board[4]}   ||  ${board[5]}  ||  ${board[6]}  |\n-------------------\n|  ${board[1]}  ||  ${board[2]}  ||  ${board[3]}  |"
+function displayBoard() {
+  echo -e "****** Tic-Tac-Toe ******* \n|  ${board[1]}  ||  ${board[2]}  ||  ${board[3]}  |\n---------------------\n| ${board[4]}   ||  ${board[5]}  ||  ${board[6]}  |\n-------------------\n|  ${board[7]}  ||  ${board[8]}  ||  ${board[9]}  |"
 
 }
+
+function tossToPlay() {
+    if [ $((RANDOM%2)) -eq 0 ]
+    then
+    switchPlayer=0
+    echo "Player will play first."
+    read -p "choose your letter X or O : " l
+      if [[ $l == $computerLetter ]]
+      then
+         playerLetter=$l
+         computerLetter=O
+      else
+         playerLetter=$l
+         computerLetter=X
+      fi
+    else
+       echo "Computer will play first."
+    fi
+}
+
 resettingBoard
 displayBoard
+tossToPlay
